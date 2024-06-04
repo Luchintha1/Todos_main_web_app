221309fc6ddb3a5b1bd9087d0b71b66bb1c8f7e9
Luchintha <luchinthaa@gmail.com>
2024-06-04 23:09:52 +0530

    Web version 0.1v

---
+++ pythonProject/webApp/Functions.py
@@ -0,0 +1,16 @@
+# Creating a method
+def get_todos(filePath="todos.txt"):
+
+    """ Read the text file and return a list """
+
+    with open(filePath, 'r') as read_file:
+        todo_list_local = read_file.readlines()
+    return todo_list_local
+
+
+def set_todos(todos_arg, filePath="todos.txt"):
+
+    """ Write the text file and add todos to the list """
+
+    with open(filePath, 'w') as write_file:
+        write_file.writelines(todos_arg)
---
+++ pythonProject/webApp/web.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import Functions
+
+todos = Functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    if len(todo) != 0:
+        todos.append(todo)
+
+    Functions.set_todos(todos)
+
+
+st.title("To-Do Web App")
+st.subheader('ToDos')
+st.write('This app increase your productivity')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        Functions.set_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="Todo", label_visibility='hidden', placeholder='Add a new todo...',
+              on_change=add_todo, key="new_todo")
